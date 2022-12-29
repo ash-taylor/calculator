610f689e663fb1772f6ecc246ef2cf46b3d22cd5
Ashley Taylor <ashtaylor2010@gmail.com>
2022-12-29 14:47:02 +0000

    HTML, JS and CSS files created

---
+++ script.js
@@ -0,0 +1,15 @@
+// script to go here
+
+// const add = (a, b) => a + b;
+
+// const subtract = (a, b) => a - b;
+
+// const sum = array => array.reduce((total, item) => total + item, 0);
+
+// // const multiply = array => array.reduce((total, item) => total * item);
+
+// const multiply = array => array.length ? array.reduce((total, item) => total * item) : 0; // Adds ternary to test for empty array - if no array, returns 0
+
+// const power = (a, b) => a ** b;
+
+// const factorial = n => n === 0 ? 1 : n * factorial(n-1);
